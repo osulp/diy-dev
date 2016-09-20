15f7598ab5c0b7ece1d53c0608fd7c993a2ae84e
mickeroo <michael.eaton@oregonstate.edu>
2016-09-20 11:06:26 -0700

    Removes OSUL search module.

--- index.php
+++ index.php
@@ -16,6 +16,8 @@
  */
 define('DRUPAL_ROOT', getcwd());
 
+ini_set('xdebug.max_nesting_level', 200);
+
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
